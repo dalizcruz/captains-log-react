1968235039cb571ad15271aa1cf4c32e789f269b
vrwatson <vanessawatson@pursuit.org>
2021-06-12 18:06:33 -0400

    react app navbar setup and connect to backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import NavBar from "./Components/NavBar";
+
+import { apiURL } from "./util/apiURL";
+const API = apiURL();
+
 function App() {
-  return <div>Hello World</div>;
+
+  const [logs, setLogs] = useState([]);
+
+  const fetchLogs = async () => {
+    let res;
+    try {
+      res = await axios.get(`${API}/logs`)
+      console.log(res.data)
+      setLogs(res.data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchLogs();
+  })
+
+  return (
+    <div>
+      <Router>
+      <NavBar logs={logs} />
+      <main>
+        <p>Captain's Log</p>
+        <Switch>
+          <Route path="/logs/:index" />
+          {/* <Route exact path="/logs" />
+        <Route path="/logs/new"/> */}
+        </Switch>
+      </main>
+      
+      
+      </Router>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Components/Logs.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Logs = ({ 
+    // "PROPS (CAPTAIN'S LOG) FROM APP.JS" 
+}) => {
+    return(
+        <div>
+            {/* "MAP THROUGH LOGS, CREATE LIST AND LINK EACH LI TO LOGS/:INDEX" */}
+        </div>
+    )
+}
+export default Logs;
